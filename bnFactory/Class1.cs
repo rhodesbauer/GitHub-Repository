d30e05f81a29df272d1a2cc43257349bb4b12e1d
rhodesbauer <rhodes.bauer@gmail.com>
2018-05-18 19:46:50 -0300

    initial setup

---
+++ bnFactory/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace bnFactory
+{
+    public class Class1
+    {
+    }
+}
---
+++ bnUI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace bnUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
